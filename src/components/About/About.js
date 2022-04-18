63cc671ccc5a68ffd4820d8c9233e1f0a8adad92
withnahidul <nahidulislam6800@gmail.com>
2022-04-18 22:54:38 +0600

    edit-aboutt

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,17 +3,23 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h2>This is about me page</h2>
+      <h2>About Me,</h2>
       <h2 className="text-3xl">Md. Nahidul Islam</h2>
       <h4>withnahidul@gmail.com</h4>
       <h4>Education: BSc in Cse </h4>
+      <h4>Skils:</h4>
+      <li>React</li>
+      <li>Java Script</li>
+      <li>Seo</li>
+      <li>Tailwind css</li>
       <p>
         My goal Be a Developer.I, Md. Nahidul Islam, do hereby declare that the
         information contained herein is true and correct to the best of my
         knowledge and belief.Daily i worked hard to full fill my goal.i'm going
         to finished programming hero course.Its a nice course for me to learn
-        web development.I also build my nwtwork daily on line and offline. Every early morning I practice code with frsh mind.Pray
-        for me may Allah will bless me.
+        web development.I also build my nwtwork daily on line and offline. Every
+        early morning I practice code with frsh mind.Pray for me may Allah will
+        bless me.
       </p>
     </div>
   );
