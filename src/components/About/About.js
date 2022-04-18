5335130551fe86e85ab46aff067902e586fa5cdb
withnahidul <nahidulislam6800@gmail.com>
2022-04-18 22:40:35 +0600

    about-me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h2>This is about me page</h2>
-            <h2 className='text-3xl'>Md. Nahidul Islam</h2>
-            <h4>withnahidul@gmail.com</h4>
-            <h4>Education</h4>
-
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is about me page</h2>
+      <h2 className="text-3xl">Md. Nahidul Islam</h2>
+      <h4>withnahidul@gmail.com</h4>
+      <h4>Education: BSc in Cse </h4>
+      <p>
+        My goal Be a Developer.I, Md. Nahidul Islam, do hereby declare that the
+        information contained herein is true and correct to the best of my
+        knowledge and belief.Daily i worked hard to full fill my goal.i'm going
+        to finished programming hero course.Its a nice course for me to learn
+        web development. Every early morning I practice code with frsh mind.Pray
+        for me may Allah will bless me.
+      </p>
+    </div>
+  );
 };
 
 export default About;
