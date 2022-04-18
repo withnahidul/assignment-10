80df9ac69d549029272f4e32a5812180700dfbce
withnahidul <nahidulislam6800@gmail.com>
2022-04-18 17:54:39 +0600

    qustion-answer

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,7 +4,9 @@ const Blogs = () => {
   return (
     <div>
       <div>
-        <h2 className="text-4xl">1.Difference between authorization and authentication?</h2>
+        <h2 className="text-4xl">
+          1.Difference between authorization and authentication?
+        </h2>
         <p>
           Authentication: -Checks the persons identity to grant access to the
           system. -Its verifying user's Credentials(user name and password). -It
@@ -16,12 +18,47 @@ const Blogs = () => {
         </p>
       </div>
       <div>
-        <h2></h2>
-        <p></p>
+        <h2 className="text-4xl">
+          2. Why are you using firebase? What other options do you have to
+          implement authentication?
+        </h2>
+        <p>
+          With development environment like Goole firebase this is can speed up
+          the backend development and focus on apps frontend development. There
+          are main 3 services I like:
+          <li>
+            i)Apps development: firebase ML/Firebase Auth/Cloud storage/Realtime
+            Database.
+          </li>
+          <li>
+            ii)Quality Improvement: Firebase crashlytics/Performance Monitoring
+            Tools/Firebase test lab
+          </li>
+          <li>
+            iii) Instrument for Business group : Firebase in-app
+            messaging/Firebase A/B Testing{" "}
+          </li>
+          <h2>There are to many authentication types we can use:</h2>
+          <li>-Thought cards</li>
+          <li>Retina scan</li>
+          <li>Voice Recognition</li>
+          <li>Biometric</li>
+          <li>Phone number etc.</li>
+        </p>
       </div>
       <div>
-        <h2></h2>
-        <p></p>
+        <h2 className="text-4xl">
+          3. What other services does firebase provide other than
+          authentication?
+        </h2>
+        <p>
+          <h4>Firebase provide lot of services:</h4>
+          <li>It provide Realtime database</li>
+          <li>It provide Cloud storage</li>
+          <li>It provide Test lab </li>
+          <li>It provide Performance Monitoring Tools</li>
+          <li>It provide in-app messaging for business.</li>
+        </p>
       </div>
     </div>
   );
