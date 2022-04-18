34b83c8dbaa6d03bbdc9286675bad445f55197fd
withnahidul <nahidulislam6800@gmail.com>
2022-04-18 22:05:49 +0600

    edit-about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,10 @@ const About = () => {
     return (
         <div>
             <h2>This is about me page</h2>
+            <h2 className='text-3xl'>Md. Nahidul Islam</h2>
+            <h4>withnahidul@gmail.com</h4>
+            <h4>Education</h4>
+
         </div>
     );
 };
